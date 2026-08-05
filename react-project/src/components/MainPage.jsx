ed5f11fe294fe79f252cec210911fb5a890275a3
udaypavanappala <udaypavan.appala@gmail.com>
2026-08-05 11:35:01 +0530

    react practice project

---
+++ react-project/src/components/MainPage.jsx
@@ -0,0 +1,15 @@
+ function Main() {
+  return (
+    <>
+      <h1>Reasons to learn react</h1>
+      <ol>
+        <li>React is Trending technologies in current frontend development</li>
+        <li>It is component based language</li>
+        <li>React has large community support</li>
+        <li>easy to develop and deploy</li>
+      </ol>
+    </>
+  );
+}
+
+export default Main;
---
+++ react-project/src/components/Page.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header"
+import Main from "./MainPage";
+import Footer from "./Footer";
+
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default Page;
